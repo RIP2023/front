099612134b8bee0f7e0aaeca15bd7934c297ac41
Artem <vegard030@gmail.com>
2023-12-30 15:47:35 +0300

    Upload_to_org

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-base: "/rip_frontend/",
   plugins: [react()],
+  server: {
+    host: '127.0.0.1',
+    port: 3000,
+  }
 })
